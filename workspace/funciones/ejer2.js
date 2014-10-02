9ba3374e3e1f0e2ae629ba679147ad7061866873
amaiafdez <afernandezvi13dw@ikzubirimanteo.com>
2014-10-02 13:30:17 +0200

    v1

---
+++ workspace/funciones/ejer2.js
@@ -0,0 +1,17 @@
+function esMayusMin(palabra) {
+
+	if(letras == letras.toUpperCase()){
+		
+		document.write(letras + " está en mayusculas");
+	}else if(letras == letras.toLowerCase()){
+		document.write(letras + " está en minusculas");
+	}else{
+		document.write(letras + " está en mayusculas y minusculas");
+	}
+				
+				
+}
+			
+esMayusMin("Hola");
+esMayusMin("HOLA");
+esMayusMin("hola");
